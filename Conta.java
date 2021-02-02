133f4dccfb1c0b7360541ce7b0947fd11ad5e1d6
Tboias <candidobr12@gmail.com>
2021-02-02 18:51:10 -0300

    Adicionando Contas

---
+++ Conta.java
@@ -0,0 +1,52 @@
+public class Conta {
+
+    private double saldo;
+    private int agencia;
+    private int numero;
+    private Cliente titular;
+
+    public double getSaldo() {
+        return this.saldo;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int novoNumero) {
+        this.numero = novoNumero;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(int novaAgencia) {
+        this.numero = novaAgencia;
+    }
+
+    public boolean depositar(double valor) {
+        this.saldo += valor;
+        return true;
+    }
+
+    public boolean sacar(double valor) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean transferir(double valor, Conta destino) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+            destino.saldo += valor;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
